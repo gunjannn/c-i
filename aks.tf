4202e29203ac687c0264b3d745bebed171b724d9
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-07 05:02:14 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,1 +1,35 @@
+provider "azurerm" {
+  version = "=2.9.0"
+  features {}
+}
 
+resource "azurerm_resource_group" "azkubernetes" {
+  name     = "azkubernetes"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "gitops-demo-aks"
+  location            = azurerm_resource_group.azkubernetes.location
+  resource_group_name = azurerm_resource_group.azkubernetes.name
+  dns_prefix          = "gitlab"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_F2s_v2"
+    os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Terraform = "True"
+  }
+}
+
+output "env-dynamic-url" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+}
