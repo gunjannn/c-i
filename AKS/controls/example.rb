c927138b95f785effc163dbf1b7d21b93d47453d
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-07 05:56:12 +0530

    Update example.rb

--- AKS/controls/example.rb
+++ AKS/controls/example.rb
@@ -9,25 +9,41 @@
 #  its ('tags'){ should cmp 'Terraform' }
 #  end
 
-  
-  
-control 'azurerm_aks_clusters' do
+ control 'azurerm_aks_clusters' do
   impact 'critical'
   title 'aks_cluster: configure Kubernetes cluster'
   describe azurerm_aks_cluster(resource_group: 'azkubernetes', name: 'gitops-demo-aks') do
   it { should exist }
-  it { should be running}
   its('location') { should cmp 'eastus' }
   its('resource_group_name') { should cmp 'azkubernetes' }
-  its ('tags'){ should cmp 'Terraform=True' }
+  its ('tags'){ should cmp 'Terraform : True' }
   its('properties.dnsPrefix') { should cmp 'gitlab' }
   its('properties.agentPoolProfiles.first.name') { should cmp 'default' }
   its('properties.agentPoolProfiles.first.count') { should cmp 1 }
   its('properties.agentPoolProfiles.first.vmSize') { should cmp 'Standard_F2s_v2' }
-  its('properties.osProfile.diskSizeGB') { should cmp 30 }
   its('properties.provisioningState') { should cmp 'Succeeded' }
+  its('properties.agentPoolProfiles.first.osType') { should cmp 'Linux' }
+  its('properties.agentPoolProfiles.first.osProfile.diskSize') { should cmp >=30 }
   end
-end  
+end 
+  
+# control 'azurerm_aks_clusters' do
+#   impact 'critical'
+#   title 'aks_cluster: configure Kubernetes cluster'
+#   describe azurerm_aks_cluster(resource_group: 'azkubernetes', name: 'gitops-demo-aks') do
+#   it { should exist }
+#   it { should be running}
+#   its('location') { should cmp 'eastus' }
+#   its('resource_group_name') { should cmp 'azkubernetes' }
+#   its ('tags'){ should cmp 'Terraform=True' }
+#   its('properties.dnsPrefix') { should cmp 'gitlab' }
+#   its('properties.agentPoolProfiles.first.name') { should cmp 'default' }
+#   its('properties.agentPoolProfiles.first.count') { should cmp 1 }
+#   its('properties.agentPoolProfiles.first.vmSize') { should cmp 'Standard_F2s_v2' }
+#   its('properties.osProfile.diskSizeGB') { should cmp 30 }
+#   its('properties.provisioningState') { should cmp 'Succeeded' }
+#   end
+# end  
 
 
 
