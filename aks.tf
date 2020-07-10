24846f6602e6b25dad827c7280f5d0d0787f2d9d
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-11 02:53:49 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
 
-output "env-dynamic-url" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+   output "env-dynamic-url" {
+     value = azurerm_kubernetes_cluster.aks.kube_config.0.host
 }
   }
