cdb4591fbbb92e41a7ca2bd2d17b7e35c2569198
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-11 00:15:22 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   version = "=1.44.0"
   
-  se  rvice_principal {
+  service_principal {
     subscription_id = "64b70538-bc40-4492-9c4b-13f8b43e732d"
     tenant_id = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
     client_id     = "e69a80e6-8258-4f1a-9bdc-e82cd6128036" # defaults to msi 
