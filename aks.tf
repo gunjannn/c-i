87dd71d86b4c3d3afa316f9c6b67783cc89476e8
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-11 02:35:54 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,13 +1,6 @@
-
-
 provider "azurerm" {
-  version = "=1.44.0"
-  subscription_id = "64b70538-bc40-4492-9c4b-13f8b43e732d"
-  client_id       = "9f165633-0fb1-46ed-9a89-acca8547244c"
-  client_secret   = "I7Xd.2RbIWV1o6Ip9k.S-Q6JsQmD~QJVK_"
-  tenant_id       = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
+  /*version = "=1.44.0"*/
 
- 
   features {}
 }
 
@@ -29,18 +22,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
     os_disk_size_gb = 30
   }
 
-  /*identity {
+  identity {
     type = "SystemAssigned"
-  }*/
+    }
 
-  service_principal {
-    client_id = "3df2bfee-ee00-4516-bebb-693639119968"
-    client_secret = "1DnE_p3Mm1VTa7garwN7nynbDXXN1LCCLj"
-  }
   tags = {
     Terraform = "True"
   }
-}
+
 
 output "env-dynamic-url" {
   value = azurerm_kubernetes_cluster.aks.kube_config.0.host
