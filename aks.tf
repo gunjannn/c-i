269b7fa9603fa9d5265b28500eae134604d1d0dd
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-12 03:21:21 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -36,7 +36,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Terraform = "True"
   }
   
-  output env-dynamic-url {
+  output "env-dynamic-url" {
      value = azurerm_kubernetes_cluster.aks.kube_config.0.host
 }
   }
