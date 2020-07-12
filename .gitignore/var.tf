e3bf6f152719a60331a430052dcaae33d4b556c0
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-12 17:17:43 +0530

    Update var.tf

--- .gitignore/var.tf
+++ .gitignore/var.tf
@@ -1,19 +1,19 @@
-variable "ARM_CLIENT_ID" {
+variable "client_id" {
     description = "client"
     default = "4aa4edf1-b7a2-4718-b12d-207802db30ca"
 }
 
-variable "ARM_SUBSCRIPTION_ID" {
+variable "subscription_id" {
     description = "subscription"
     default = "64b70538-bc40-4492-9c4b-13f8b43e732d"
 }
 
-variable "ARM_CLIENT_SECRET" {
+variable "client_secret" {
     description = "secret"
     default = "JBsa-QdcTmg7PCG5hC2xwCxvvwcPA-sfpY"
 }
 
-variable "ARM_TENANT_ID" {
+variable "tenant_id" {
     description = "tenant"
     default = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
 }
