720fdc6143eaa33d1bf1661d29470c52903df93b
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-12 17:28:07 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${module.vaultprovider.client_id}"
-  client_secret   = "${module.vaultprovider.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = "var.subscription_id"
+  client_id       = "var.client_id"
+  client_secret   = "var.client_secret"
+  tenant_id       = "var.tenant_id"
 
   version =  "~> 2.18"
   features {}
