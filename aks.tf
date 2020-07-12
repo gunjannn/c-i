f6329e1d68f17db4d425d523ed8daa4898a724cb
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-12 05:46:00 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
   version =  "~> 2.18"
   
-client_id = "4aa4edf1-b7a2-4718-b12d-207802db30ca"
+/*client_id = "4aa4edf1-b7a2-4718-b12d-207802db30ca"
 client_secret = "JBsa-QdcTmg7PCG5hC2xwCxvvwcPA-sfpY"
 tenant_id = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
 subscription_id = "64b70538-bc40-4492-9c4b-13f8b43e732d"
-
+*/
 
   features {}
 }
