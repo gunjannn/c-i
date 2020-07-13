220b0b1b741d76751e26d1bdeb7fb2a7f79327cc
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-13 16:30:51 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,19 +1,19 @@
-variable "client_id" {
+variable "AZURE_CLIENT_ID" {
     description = "client"
-    default = "4aa4edf1-b7a2-4718-b12d-207802db30ca"
+    default = "az"
 }
 
-variable "subscription_id" {
+variable "AZURE_SUBSCRIPTION_ID" {
     description = "subscription"
-    default = "64b70538-bc40-4492-9c4b-13f8b43e732d"
+    default = "az"
 }
 
-variable "client_secret" {
+variable "AZURE_CLIENT_SECRET" {
     description = "secret"
-    default = "JBsa-QdcTmg7PCG5hC2xwCxvvwcPA-sfpY"
+    default = "az"
 }
 
-variable "tenant_id" {
+variable "AZURE_TENANT_ID" {
     description = "tenant"
-    default = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
+    default = "az"
 }
