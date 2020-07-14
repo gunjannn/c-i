39ff58782d2156fe74b6ac0c003ee8cc27a7043d
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-14 12:38:14 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,19 +1,19 @@
 variable "AZURE_CLIENT_ID" {
     description = "client"
-    default = "az"
+    default = "4aa4edf1-b7a2-4718-b12d-207802db30ca"
 }
 
 variable "AZURE_SUBSCRIPTION_ID" {
     description = "subscription"
-    default = "az"
+    default = "64b70538-bc40-4492-9c4b-13f8b43e732d"
 }
 
 variable "AZURE_CLIENT_SECRET" {
     description = "secret"
-    default = "az"
+    default = "JBsa-QdcTmg7PCG5hC2xwCxvvwcPA-sfpY"
 }
 
 variable "AZURE_TENANT_ID" {
     description = "tenant"
-    default = "az"
+    default = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
 }
