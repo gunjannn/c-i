82b31b6cd0c297ab70af0ebe3c7dd583af29e7e9
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-14 20:12:58 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,24 @@
+
+variable "client_id" {
+  description = "The Client ID (appId) for the Service Principal used for the AKS deployment"
+}
+
+variable "client_secret" {
+  description = "The Client Secret (password) for the Service Principal used for the AKS deployment"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for the Service Principal used for the AKS deployment"
+}
+
+variable "tenant_id" {
+  description = "The Tenant Id for the Service Principal used for the AKS deployment"
+}
+
+
+
+
+
 /*variable "AZURE_CLIENT_ID" {
     description = "client"
     default = "4aa4edf1-b7a2-4718-b12d-207802db30ca"
