2b764669528f5a08515d92f3d1d6c72560e23718
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-17 17:19:57 +0530

    Update docker.rb

--- AKS/controls/docker.rb
+++ AKS/controls/docker.rb
@@ -1,3 +1,7 @@
+
+control 'azurerm_aks_cluster' do
+  impact 'critical'
+  title 'aks_cluster: configure Kubernetes cluster'
 describe docker.version do
   its('Server.Version') { should cmp >= '1.12'}
   its('Client.Version') { should cmp >= '1.12'}
